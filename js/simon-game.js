3dc9224b70cf7f51eb018281aa8aa244be0c54bb
Promie Yutasane <promie.yutasane@gmail.com>
2017-12-14 20:17:19 +1100

    First part of logic mapped

---
+++ js/simon-game.js
@@ -0,0 +1,19 @@
+
+
+//Creat ID's for each colour on the HTML
+
+//Add a click event to each ID's and test it with console log to ensure that each function is working when clicked
+
+/*
+
+Building a sequence
+
+1. Red
+2. Red, Blue, 
+3. Red, Blue, Yellow
+
+Push to an empty array?
+
+*/
+
+// Random colour - Math.floor(Math.random() * 4) use it against an array
