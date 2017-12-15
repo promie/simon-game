e645ad60986e71169ab5249015fc5fb55b336c81
Promie Yutasane <promie.yutasane@gmail.com>
2017-12-16 09:45:53 +1100

    First part of the functionality working

--- js/simon-game.js
+++ js/simon-game.js
@@ -1,28 +1,40 @@
 
+const greenButton = document.getElementById('green');
+const redButton = document.getElementById('red');
+const yellowButton = document.getElementById('yellow');
+const blueButton = document.getElementById('blue');
+const RED = 'RED';
+const BLUE = 'BLUE';
+const YELLOW = 'YELLOW';
+const GREEN = 'GREEN';
 
-//Creat ID's for each colour on the HTML
 
-//Add a click event to each ID's and test it with console log to ensure that each function is working when clicked
+const simonObject = {
+    sendColour: function(colour){
+        console.log(`New Colour: ${colour}`)
+    }
+}
 
-/*
 
-Building a sequence
+//Click Events
+greenButton.addEventListener('click', function(){
+    simonObject.sendColour(GREEN);
+});
 
-1. Red
-2. Red, Blue, 
-3. Red, Blue, Yellow
+red.addEventListener('click', function(){
+    simonObject.sendColour(RED);
+});
 
-Push to an empty array?
+yellow.addEventListener('click', function(){
+    simonObject.sendColour(YELLOW);
+});
 
-*/
+blue.addEventListener('click', function(){
+    simonObject.sendColour(BLUE);
+});
 
-// Random colour - Math.floor(Math.random() * 4) use it against an array - or the length of the array instead of the static number.
 
-//Push random colour into an empty array
 
-//if simon length is equal to zero then start a new game
-//else if go to next step if selected correctly --> if the sequence === length of array -1 then clear the sequence back to zero and call the next sequence else increment the steps.
-// and then if lose --> alert wrong!, sequence = [] and step back to 0;
 
 
 
