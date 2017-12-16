a56763620136007d507f9ccdff921923bc2ee130
Promie Yutasane <promie.yutasane@gmail.com>
2017-12-16 11:05:43 +1100

    Sound functioning

--- js/simon-game.js
+++ js/simon-game.js
@@ -1,12 +1,16 @@
 
-const greenButton = document.getElementById('green');
-const redButton = document.getElementById('red');
-const yellowButton = document.getElementById('yellow');
-const blueButton = document.getElementById('blue');
-const RED = 'RED';
-const BLUE = 'BLUE';
-const YELLOW = 'YELLOW';
-const GREEN = 'GREEN';
+const greenButton = document.getElementById('green'),
+    redButton = document.getElementById('red'),
+    yellowButton = document.getElementById('yellow'),
+    blueButton = document.getElementById('blue'),
+    greenSound = document.getElementById('green-sound'),
+    redSound = document.getElementById('red-sound'),
+    yellowSound = document.getElementById('yellow-sound'),
+    blueSound = document.getElementById('blue-sound'),
+    RED = 'RED',
+    BLUE = 'BLUE',
+    YELLOW = 'YELLOW',
+    GREEN = 'GREEN';
 
 
 const simonObject = {
@@ -19,19 +23,44 @@ const simonObject = {
 //Click Events
 greenButton.addEventListener('click', function(){
     simonObject.sendColour(GREEN);
+    playSound(GREEN);
 });
 
 red.addEventListener('click', function(){
     simonObject.sendColour(RED);
+    playSound(RED);
 });
 
 yellow.addEventListener('click', function(){
     simonObject.sendColour(YELLOW);
+    playSound(YELLOW);
 });
 
 blue.addEventListener('click', function(){
     simonObject.sendColour(BLUE);
+    playSound(BLUE);
 });
+
+//play-sound Functions
+const playSound = (colour) =>{
+    
+    switch(colour){
+        case 'GREEN':
+            greenSound.play();
+            break;
+        case 'RED':
+            redSound.play();
+            break;
+        case 'YELLOW':
+            yellowSound.play();
+            break;
+        case 'BLUE':
+            blueSound.play();
+            break;
+        default:
+            break;
+    }
+}
 
 
 
